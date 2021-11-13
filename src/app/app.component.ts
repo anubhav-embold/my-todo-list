51499bdd8deb49d2554bbe1005d51b752c6b7a65
Anubhav Kandiyal <anubhavc9@gmail.com>
2021-11-13 15:19:28 +0530

    removed comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-todo-list';
-  constructor(){
-  // setTimeout(() => {
-  //   this.title = "Changed title";
-  // }, 2000);
-}
+  constructor() {}
 }
